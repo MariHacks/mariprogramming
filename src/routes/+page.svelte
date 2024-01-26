ec5bcbe4b3bcaf3fdc680e752d0d9058eecc19e0
Carolyn Wu <carolyn94118@gmail.com>
2024-01-25 20:56:41 -0500

    chore: update events and roadmap for w24

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,10 +24,12 @@
 
 <div class="events-hero px-sm-1 py-5 overflow-x-hidden relative">
   <h3 class="text-center fw-bold mb-4 text-black">Upcoming events</h3>
-  <EventCarousel events={{'Join a club day': ['TBD, 2024', 'Come see us in the gym during AP to learn more about the club and to sign up!'], 
-                          'Workshop: Algorithms and Data Structures 1': ['Jan 23, 2024 (TBC)', 'Now that we\'ve covered Python basics, we will be covering foundational concepts in programming: algorithms and data structures.'],
-                          'Workshop: Algorithms and Data Structures 2': ['Jan 30, 2024 (TBC)', 'A continuation of last week\'s workshop on algorithms and data structures.'],
-                          'MariHacks registration opens': ['Feb 2024 (TBD)', 'High school and CEGEP students: make sure to register for MariHacks, the official hackathon of Marianopolis College!'],
+  <EventCarousel events={{'Join a club day': ['Feb 1, 2024', 'Come see us in the gym during AP to learn more about the club and to sign up!'], 
+                          'MariHacks mentor, judge, volunteer, panelist registration opens': ['Feb 12, 2024 or earlier (TBC)', 'High school and CEGEP students: make sure to register for MariHacks, the official hackathon of Marianopolis College!'],
+                          'MariHacks hacker registration opens': ['Feb 12, 2024', 'High school and CEGEP students: make sure to register for MariHacks, the official hackathon of Marianopolis College!'],
+                          'Workshop: OOP': ['Feb 13, 2024 (TBC)', 'Now that we\'ve covered Python basics, we will be covering the object-oriented programming (OOP) paradigm!'],
+                          'Workshop: Algorithms and Data Structures 1': ['Feb 20, 2024 (TBC)', 'Knowing OOP will now allow us to learn fundamental computer science and programming concepts: algorithms and data structures.'],
+                          'Workshop: Algorithms and Data Structures 2': ['Feb 29, 2024 (TBC)', 'A continuation of last week\'s workshop on algorithms and data structures.'],
                           'MariHacks': ['Apr 5-6, 2024', 'The 7th edition of Marianopolis College\'s official annual hackathon! See the MariHacks website for more details.']}} />
 </div>
 
