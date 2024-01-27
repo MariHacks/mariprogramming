576dda6f8b1be9c187bda2da0bdd4024945541bc
Carolyn Wu <carolyn94118@gmail.com>
2024-01-27 13:06:33 -0500

    chore(home): fix mentor/judge/vol/panelist registration desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
 <div class="events-hero px-sm-1 py-5 overflow-x-hidden relative">
   <h3 class="text-center fw-bold mb-4 text-black">Upcoming events</h3>
   <EventCarousel events={{'Join a club day': ['Feb 1, 2024', 'Come see us in the gym during AP to learn more about the club and to sign up!'], 
-                          'MariHacks mentor, judge, volunteer, panelist registration opens': ['Feb 12, 2024 or earlier (TBC)', 'High school and CEGEP students: make sure to register for MariHacks, the official hackathon of Marianopolis College!'],
+                          'MariHacks mentor, judge, volunteer, panelist registration opens': ['Feb 12, 2024 or earlier (TBC)', 'CEGEP and university students, computer science industry professionals and more: we\'d love to have you at MariHacks this year as a volunteer, judge, mentor and/or guest speaker! Sign up or see more details on the MariHacks website!'],
                           'MariHacks hacker registration opens': ['Feb 12, 2024', 'High school and CEGEP students: make sure to register for MariHacks, the official hackathon of Marianopolis College!'],
                           'Workshop: OOP': ['Feb 13, 2024 (TBC)', 'Now that we\'ve covered Python basics, we will be covering the object-oriented programming (OOP) paradigm!'],
                           'Workshop: Algorithms and Data Structures 1': ['Feb 20, 2024 (TBC)', 'Knowing OOP will now allow us to learn fundamental computer science and programming concepts: algorithms and data structures.'],
