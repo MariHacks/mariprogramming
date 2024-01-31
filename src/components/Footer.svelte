98fe632359feb173051cb2aae68d3be6cce995a1
cw118 <carolyn94118@gmail.com>
2024-01-31 10:12:38 -0500

    feat(footer): update to new MariHacks logo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
     <a class="text-white" href="https://github.com/MariHacks"><img src="/socials/github.svg" height="30" alt="GitHub logo"><span class="ms-2 d-none d-md-inline">GitHub</span></a>
     <a class="text-white" href="https://discord.gg/BMvrpKJjej"><img src="/socials/discord.svg" height="30" alt="Discord logo"><span class="ms-2 d-none d-md-inline">Discord</span></a>
     <a class="text-white" href="https://www.instagram.com/mari_programming_club/"><img src="/socials/instagram.svg" height="30" alt="Instagram logo"><span class="ms-2 d-none d-md-inline">Instagram</span></a>
-    <a class="text-white" href="https://www.marihacks.com/"><img src="/logo-icon.svg" height="30" alt="MariHacks logo"><span class="ms-2 d-none d-md-inline">MariHacks</span></a>
+    <a class="text-white" href="https://www.marihacks.com/"><img src="/socials/marihacks.png" height="30" alt="MariHacks logo"><span class="ms-2 d-none d-md-inline">MariHacks</span></a>
   </div>
   <div class="px-4 py-2 bar">&#60;The Marianopolis Programming Club&#160;/&#62;</div>
 </footer>
